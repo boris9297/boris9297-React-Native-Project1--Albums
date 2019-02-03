db19288d5bab64d1901caa549f20865bee86c734
Boris <boris9297@gmail.com>
2019-02-02 19:34:30 -0800

    section8 finished

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
 	// const { textStyle, viewStyle } = styles;
 	return (
-		<View>
-			<Text>{props.record.title}</Text>
-		</View>
+		<Card>
+			<CardSection>
+				<Text>{props.record.title}</Text>
+			</CardSection>
+		</Card>
 	);
 };
 export default AlbumDetail;
